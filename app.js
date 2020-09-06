65b0911463e7e541ae6d0fc5968edb73f0ef289c
yiping <qwer951212@gmail.com>
2020-09-06 23:54:43 +0800

    Add init project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateTrashTalk = require('./generate_trash_talk')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const trashTalk = generateTrashTalk(req.body.target)
+  res.render('index', { trashTalk: trashTalk })
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
